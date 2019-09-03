def1026516b9170c80a56199ca683663790be1ee
kaungsattkhant <ookaung42@gmail.com>
2019-09-03 16:55:00 +0630

    image upload successful

--- app/src/main/java/com/chann/tipster/api/ApiEnd.java
+++ app/src/main/java/com/chann/tipster/api/ApiEnd.java
@@ -1,12 +1,17 @@
 package com.chann.tipster.api;
 
+import com.chann.tipster.data.EdiProfile;
 import com.chann.tipster.data.Login;
 import com.chann.tipster.data.Register;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public interface ApiEnd {
 
@@ -18,6 +23,9 @@ public interface ApiEnd {
     @POST("/api/register")
     Call<Register> userRegister(@Field("phone_number") String phoneNumber, @Field("password") String password);
 
+    @Multipart
+    @POST("/api/profile_edit")
+    Call<EdiProfile> profileEdit(@Part("token") RequestBody token , @Part("name") RequestBody name , @Part MultipartBody.Part image);
 
 
 }
---
+++ app/src/main/java/com/chann/tipster/data/EdiProfile.java
@@ -0,0 +1,6 @@
+package com.chann.tipster.data;
+
+public class EdiProfile {
+
+    public boolean isSuccess;
+}
